09b087e8042c8bf901e01e7c29515745cd50e88a
justultra <141536692+justultra@users.noreply.github.com>
2025-09-05 16:22:00 -0400

    Fetching weather data from api

--- weather.js
+++ weather.js
@@ -1,3 +1,13 @@
 const API_KEY = "5d17e4ca235a2738ddaeb14a9f414fa9";
 const CITY = "Worcester";
 
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=imperial`)
+    .then(response => response.json())
+    .then(data => {
+        const TEMP = data.main.temp;
+        const WEATHER = data.weather[0].description;
+        const weatherDiv = document.getElementById('weatherDiv');
+        weatherDiv.innerHTML = `<p>The current temperature in ${CITY} is ${TEMP}°F with ${WEATHER}.</p>`;
+    }
+)
+    .catch(error => console.error('Error fetching weather data:', error));
