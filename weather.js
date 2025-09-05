cb568ef651a55f049e059255bbc8705b3254775e
justultra <141536692+justultra@users.noreply.github.com>
2025-09-05 16:35:12 -0400

    let temp and weather before fetching data

--- weather.js
+++ weather.js
@@ -1,11 +1,14 @@
 const API_KEY = "5d17e4ca235a2738ddaeb14a9f414fa9";
 const CITY = "Worcester";
 
+let TEMP = 0;
+let WEATHER = "";
+
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=imperial`)
     .then(response => response.json())
     .then(data => {
-        const TEMP = Math.round(Number(data.main.temp));
-        const WEATHER = data.weather[0].description;
+        TEMP = Math.round(Number(data.main.temp));
+        WEATHER = data.weather[0].description;
         const weatherDiv = document.getElementById('weatherDiv');
         weatherDiv.innerHTML = `<p>The current temperature in ${CITY} is ${TEMP}°F with ${WEATHER}.</p>`;
     }
