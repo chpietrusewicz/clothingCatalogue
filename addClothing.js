59066d6e74de6bf81fc5c4bb13f391d552ed3cb6
justultra <141536692+justultra@users.noreply.github.com>
2025-09-09 09:40:20 -0400

    initial functionality of adding clothes

--- addClothing.js
+++ addClothing.js
@@ -12,11 +12,40 @@ function previewImage(event) {
     const file = event.target.files[0];
     if (file) {
         const reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             document.getElementById('output').src = e.target.result;
         };
         reader.readAsDataURL(file);
     }
+}
+
+function addClothingItem() {
+    const fileInput = document.getElementById('avatar');
+    const checkboxes = document.querySelectorAll('.tag:checked');
+    const clothingItem = {};
+
+    const file = fileInput.files[0];
+    const tags = Array.from(checkboxes).map(cb => cb.value);
+    clothingItem.tags = tags;
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            clothingItem.image = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+
+    console.log(clothingItem);
+    clearInputs();
+    closePopup();
+}
+
+function clearInputs() {
+    document.getElementById('avatar').value = '';
+    document.getElementById('output').src = '';
+    const checkboxes = document.querySelectorAll('.tag:checked');
+    checkboxes.forEach(cb => cb.checked = false);
 }
 
 document.getElementById('avatar').addEventListener('change', previewImage);
---
+++ clothing.js
@@ -0,0 +1,7 @@
+class Clothing {
+    constructor(image, color, tags) {
+        this.image = image;
+        this.color = color
+        this.tags = tags;
+    }
+}
